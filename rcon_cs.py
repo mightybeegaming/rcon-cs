6b68cc56e5c0a7887f0d68b000678f59d589f108
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-06 23:00:47 +0800

    Initial commit.

---
+++ rcon_cs.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+import time
+import re
+import configparser
+
+CS_CONFIG = configparser.ConfigParser()
+CS_CONFIG.read(r"_config-counterstrike.ini")
+
+SERVER_IP = CS_CONFIG.get("RCON", "IPAddress")
+SERVER_PORT = CS_CONFIG.getint("RCON", "Port")
+RCON_PASSWORD = CS_CONFIG.get("RCON", "Password")
+
+def get_challenge(sock):
+    packet = b"\xFF\xFF\xFF\xFFchallenge rcon\n"
+    sock.sendto(packet, (SERVER_IP, SERVER_PORT))
+
+    try:
+        data, _ = sock.recvfrom(4096)
+        text = data[4:].decode(errors="ignore")
+
+        match = re.search(r"challenge rcon (\d+)", text)
+        if match:
+            return match.group(1)
+    except socket.timeout:
+        pass
+
+    return None
+
+def send_rcon(command):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(3)
+
+    challenge = get_challenge(sock)
+    if not challenge:
+        print("Failed to get RCON challenge")
+        return
+
+    packet = (
+        b"\xFF\xFF\xFF\xFFrcon "
+        + challenge.encode()
+        + b" "
+        + RCON_PASSWORD.encode()
+        + b" "
+        + command.encode()
+        + b"\n"
+    )
+
+    sock.sendto(packet, (SERVER_IP, SERVER_PORT))
+
+    try:
+        while True:
+            data, _ = sock.recvfrom(4096)
+            print(data[4:].decode(errors="ignore"))
+    except socket.timeout:
+        pass
+
+    sock.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python rcon_cs16.py <command>")
+        sys.exit(1)
+
+    send_rcon(" ".join(sys.argv[1:]))
